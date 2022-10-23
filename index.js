a475852f0843b09e6a8d1fe99e9aaf79f53774e7
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-23 21:59:47 +0000

    Create a funciton to sum numbers recursively

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 
+// function sum(fromN, toN) {
+//   // Sum all the values from fromN up to toN
+//   if (toN === fromN) {
+//     return fromN;
+//   }
+
+//   return toN + sum(fromN, (toN - 1))
+// }
+
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if (fromN === toN) {
+    return toN;
+  }
+
+  return fromN + sum((fromN + 1), toN)
 }
+
+console.log(sum(3, 7));
 
 module.exports = sum;
